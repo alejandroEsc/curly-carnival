8fa20a61b340f65a322e001f51e57c9283b0fb9e
Joe Julian <me@joejulian.name>
2016-12-06 12:08:16 -0800

    Added distuils setup
    Added distutils setup.py for easy installation

--- README.md
+++ README.md
@@ -10,8 +10,13 @@ This node class represents a single Node with a name and weight
 
 usage:
 ```python
-from src.node import Node
+from curly_carnival.node import Node
 a = Node('node_a', 3)
 b = Node('node_b', 5)
 b.insertAfter(a)
+```
+
+installation:
+```bash
+pip install git+https://github.com/joejulian/curly-carnival.git
 ```
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='curly_carnival',
+      version='1.0',
+      description='Weighted Node List Exercise',
+      author='Joe Julian',
+      author_email='me@joejulian.name',
+      url='https://github.com/joejulian/curly-carnival',
+      package_dir={'curly_carnival': 'src'},
+      packages=['curly_carnival'],
+     )
